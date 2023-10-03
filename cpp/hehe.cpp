14193ff9d5ec3a2cd9ad7cec6dc2b541ac309a3d
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-03 18:55:32 +0700

    bruh

--- cpp/hehe.cpp
+++ cpp/hehe.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
+// #include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout << "OCom66k";
+    float a = 22/7;
+    cout << fixed << setprecision(5);
     return 0;
 }
