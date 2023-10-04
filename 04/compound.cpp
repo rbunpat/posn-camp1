407f508a76df5b48591969c00d544ff0d5038900
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-04 14:42:30 +0700

    bruh

--- 04/compound.cpp
+++ 04/compound.cpp
@@ -4,8 +4,11 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int x = 100, y = 50;
-    x += y;
-    cout << x << endl;
-    return 0;
+    // int x = 100, y = 50;
+    // x += y;
+    // cout << x << endl;
+    // return 0;
+    int x = 1, y = 3;
+    x %= y;
+    cout << x;
 }
