086beceb144575bc03ce2e878f6acca1a58cac2f
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-04 15:42:11 +0700

    bruh

--- 04/bruh.cpp
+++ 04/bruh.cpp
@@ -4,8 +4,13 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int x = 8, y = 9, z = 2;
+    int x, y, z;
+    cin >> x >> y >> z;
+    cout << x * (2 + 5) << endl;
+    //add up to 70
 
-    cout << 2 + 8 * 9 << endl;
+    // int x = 8, y = 9, z = 2;
+
+    // cout << 2 + 8 * 9 << endl;
     return 0;
 }
