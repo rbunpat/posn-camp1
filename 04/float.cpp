9de00cd746e04a84ba05726288cbc004c7578b2e
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-04 13:21:49 +0700

    bruh

---
+++ 04/float.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    float number1, number2;
+    number1 = 1.1;
+    number2 = 2.2;
+    cout << number1 << endl;
+    cout << number2 << endl;
+    return 0;
+}
