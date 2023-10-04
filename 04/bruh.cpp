437a87fc6915f500a165f00afe59d486667de719
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-04 16:22:14 +0700

    bruh

--- 04/bruh.cpp
+++ 04/bruh.cpp
@@ -4,9 +4,23 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int x, y, z;
-    cin >> x >> y >> z;
-    cout << x * (2 + 5) << endl;
+    int width, height, area;
+    cout << "Enter width: ";
+    cin >> width;
+    cout << "Enter height: ";
+    cin >> height;
+    area = width * height;
+    cout << "Area is: " << area << endl;
+    // int x, y, z;
+    // cin >> x >> y >> z;
+    // cout << x * (2 + 5) << endl;
+    // int number, num2;
+    // cout << "Input a number: ";
+    // cin >> number;
+    // cout << "Input another number: ";
+    // cin >> num2;
+    // cout << "output is " << number;
+    // cout << "number2 is " << num2 << endl;
     //add up to 70
 
     // int x = 8, y = 9, z = 2;
