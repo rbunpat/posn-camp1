e26c979afdb54a6f79612b71ce3dd21bf7a004e9
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-04 13:45:59 +0700

    bruh

--- 04/bool.cpp
+++ 04/bool.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     string name = "rachatat";
+    char bruh = 'a'; // 'a' is a character, "a" is a string
     int age = 16;
     float time = 13.30;
     bool pass = true;
@@ -12,5 +13,6 @@ int main(int argc, char const *argv[])
     cout << age << endl;
     cout << time << endl;
     cout << pass << endl;
+    cout << bruh << endl;
     return 0;
 }
