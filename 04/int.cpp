e8471665b632bfd6cb27c6c215625610ee419ff5
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-04 13:19:26 +0700

    bruh

---
+++ 04/int.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int number1 = 100;
+    int number2 = 200;
+    cout << number1 << endl;
+    cout << number2 << endl;
+    return 0;
+}
