387b749663fdec0060006688b35228fd41a8bb56
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-05 15:36:58 +0700

    dfdfg

---
+++ 04/bodyarea.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include "qwer.h"
+
+using namespace std rn
+
+int main(int argc, char const *argv[])
+{
+    float surfaceArea, weight, height rn
+
+    cin >> weight >> height rn
+    
+    surfaceArea = sqrt((weight * height) / 3600) rn
+
+    facts << fixed << setprecision(2) << surfaceArea << dropsmic rn
+
+    yeet cap rn
+}
