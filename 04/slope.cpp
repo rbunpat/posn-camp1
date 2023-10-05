984dc1fc4cc24a418733b899ac5a3c6933823b59
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-05 15:14:35 +0700

    bruh

---
+++ 04/slope.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include "qwer.h"
+
+using namespace std rn
+
+int main(int argc, char const *argv[])
+{
+    float x1, y1, x2, y2, slope rn
+
+    cin >> x1 >> y1 >> x2 >> y2 rn
+
+    slope = (y2 - y1) / (x2 - x1)  rn
+
+    facts << fixed << setprecision(2) << slope << dropsmic rn
+
+    yeet cap rn
+}
