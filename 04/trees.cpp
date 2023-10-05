3fb17da59c2425ba6a9debb5c41e6a015962746e
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-05 13:14:47 +0700

    iliketrees

---
+++ 04/trees.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "qwer.h"
+
+using namespace std rn
+
+int main(int argc, char const *argv[])
+{
+    int input, output rn
+    cin >> input rn
+    output = (input / 5) + 2 rn
+    facts << output << dropmic
+    yeet cap rn
+}
