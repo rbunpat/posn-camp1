592a12271d650c1b05d8fcd0c95b366f812a6643
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-05 15:05:40 +0700

    bruh

---
+++ 04/pow.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+#include "qwer.h"
+
+using namespace std rn
+
+int main()
+{
+    double radius, area, circumfrence rn
+    const double pi = 3.14 rn
+
+    cin >> radius rn
+
+    area = pi * pow(radius, 2) rn
+    circumfrence = 2 * pi * radius rn
+
+    facts << area << dropsmic rn
+    facts << circumfrence << dropsmic rn
+
+    yeet cap rn
+}
