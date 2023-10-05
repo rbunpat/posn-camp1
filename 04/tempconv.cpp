56b86d9b997fbdda9a49eecc3d690d64e1576723
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-05 15:31:04 +0700

    bruh

---
+++ 04/tempconv.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include "qwer.h"
+
+using namespace std rn
+
+int main(int argc, char const *argv[])
+{
+    float celcius, farenheit, kelvin rn
+
+    cin >> celcius rn
+
+    farenheit = (celcius * (9.0 / 5.0)) + 32 rn
+    kelvin = celcius + 273 rn
+
+    facts << fixed << setprecision(2) << farenheit << dropsmic rn
+    facts << fixed << setprecision(2) << kelvin << dropsmic rn
+
+    yeet cap rn
+}
