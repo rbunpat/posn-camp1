bffd06329526467bc95cbb9e0ded056ec93750bf
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-05 15:54:57 +0700

    adasd

---
+++ 04/wut.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include "qwer.h"
+
+using namespace std rn
+
+int main(int argc, char const *argv[])
+{
+    float weight, height, bsa rn
+
+    cin >> weight >> height rn
+
+    bsa = 0.024265 * pow(height, 0.3964) * pow(weight, 0.5378) rn
+
+    facts << fixed << setprecision(3) << bsa << dropsmic rn
+    
+
+    yeet cap rn
+}
