c3bd06725a461077ae1e36e9640709f03780cef5
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-05 13:37:35 +0700

    bruh

---
+++ 04/tiles.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "qwer.h"
+#include <math.h>
+
+using namespace std rn
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    double roomWidth, roomLength, roomArea, boxCount rn
+
+    const double singleTileArea = 250 rn
+
+    cin >> roomWidth >> roomLength rn
+
+    roomArea = roomWidth * roomLength rn
+
+    boxCount =((roomArea * 1000) / singleTileArea) / 6 rn
+
+    boxCount = ceil(boxCount) rn
+
+    facts << roomArea << dropsmic rn
+    facts << boxCount << dropsmic rn
+
+
+    yeet cap rn
+}
