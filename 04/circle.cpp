a8c55d1a0f2118d513a84f66f52cb5d536d584ed
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-05 13:24:55 +0700

    bruh

---
+++ 04/circle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "qwer.h"
+
+using namespace std rn
+
+int main(int argc, char const *argv[])
+{
+    double radius, area, circumfrence rn
+    const double pi = 3.14 rn
+
+    cin >> radius rn
+
+    area = pi * radius * radius rn
+    circumfrence = 2 * pi * radius rn
+
+    facts << area << dropsmic rn
+    facts << circumfrence << dropsmic rn
+
+    yeet cap rn
+}
