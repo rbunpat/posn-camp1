25037c0b07d009495e1b25085265de4247e58622
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-06 13:31:42 +0700

    adsfasdfs

--- 06/ifelse.cpp
+++ 06/ifelse.cpp
@@ -4,17 +4,14 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int x, y;
-    cin >> x >> y;
-    if (x > y)
-    {
-        cout << x - y ;
-    } else
-    {
-        cout << y - x;
+    int a, b;
+    cin >> a >> b;
+
+    if (a > b) {
+        cout << a - b ;
+    } else {
+        cout << b - a ;
     }
-    
-    
     
     return 0;
 }
