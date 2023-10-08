9836c395f89c05a44902a501056ec9ad105b6041
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-08 13:50:21 +0700

    minmax

---
+++ 08/minmax.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int numberCount, i, currentMin, currentMax, numberInput;
+
+    cin >> numberCount;
+
+    currentMin = 99999;
+    currentMax = 0;
+
+    for (i = 1; i <= numberCount; i++) {
+        cin >> numberInput;
+        if (numberInput < currentMin) {
+            currentMin = numberInput;
+        } else if (numberInput > currentMax) {
+            currentMax = numberInput;
+        }
+    }
+
+    cout << currentMin << endl;
+    cout << currentMax << endl;
+
+
+
+
+
+    return 0;
+}
