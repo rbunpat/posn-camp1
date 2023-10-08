3aaeb824e279331383ebe5b91d723b959fdb7b5d
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-08 11:29:56 +0700

    bruh

--- grader/test01/dora.cpp
+++ grader/test01/dora.cpp
@@ -6,6 +6,10 @@ int main() {
     int doraX, doraY, objectX, objectY;
     double distance1, distance2, slope;
 
+    bool checkdist;
+
+    float dist1, dist2;
+
     cin >> doraX >> doraY;
     cin >> objectX >> objectY;
 
@@ -18,6 +22,13 @@ int main() {
     // distance2 = distance1 + (distance1 * slope);
 
     distance2 = abs(objectX - doraX) + abs(objectY - doraY);
+
+    dist1 = sqrt( pow((objectX - doraX), 2) + pow((objectY - doraY), 2));
+    if (distance1 == dist1) {
+        cout << "bruh";
+    } else {
+        cout << "notbruh";
+    }
 
     cout << distance2;
 
