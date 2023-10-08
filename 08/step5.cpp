6d65acc7d3b10096185f91463ab9c3e41d3b1c19
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-08 13:22:42 +0700

    bruh

---
+++ 08/step5.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int number, i;
+
+
+
+    cin >> number;
+
+    for (i = 1; i <= number; i++) {
+        if (i % 2 != 0) {
+            cout << i << endl;
+        }
+    }
+    
+}
