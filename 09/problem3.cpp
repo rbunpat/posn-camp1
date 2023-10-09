760b4d857ff82dbc4a6b3fcaf605f8c898d50e10
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-09 10:04:57 +0700

    ssdfgsfg

--- 09/problem3.cpp
+++ 09/problem3.cpp
@@ -5,14 +5,24 @@ using namespace std;
 int main() {
     int n, numberCount;
 
+    double numberAverage, numberSum;
+
     numberCount = -1;
+    numberSum = 0;
     
     do {
         cin >> n;
         numberCount++;
+        numberSum += n;
+        
     } while (n != 0);
 
+    numberAverage = numberSum / numberCount;
+
     cout << numberCount << endl;
+    cout << numberSum << endl;
+
+    cout << numberAverage << endl;
 
     return 0;
 }
