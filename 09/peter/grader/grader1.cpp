33ee93506f774e90e831fbd52e6df59aa8057634
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-09 13:52:33 +0700

    adfadf

---
+++ 09/peter/grader/grader1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, i = 0;
+
+    cin >> n;
+
+    while (n--)
+    {
+        i++;
+        cout << i << " " ;
+    }
+    
+
+    return 0;
+}
