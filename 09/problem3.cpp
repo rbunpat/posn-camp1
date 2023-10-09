5c22964c06a2c5e8e5af455606d8d7d25e1c7dcf
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-09 09:59:18 +0700

    asdfasdf

--- 09/problem3.cpp
+++ 09/problem3.cpp
@@ -3,11 +3,16 @@
 using namespace std;
 
 int main() {
-    int n, i;
+    int n, numberCount;
+
+    numberCount = -1;
     
     do {
         cin >> n;
+        numberCount++;
     } while (n != 0);
+
+    cout << numberCount << endl;
 
     return 0;
 }
