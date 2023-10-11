49ca838fc5e617aec3762154b01ac705cc818890
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-11 13:57:34 +0700

    adfasdf

--- 10/ex09.cpp
+++ 10/ex09.cpp
@@ -2,11 +2,17 @@
 using namespace std;
 void AcceptData()
 {
-    string name, school;
+    // string name, school;
+    char name[100], school[100];
+    // char16_t name[100], school[100];
     cout << "Enter your name : ";
-    getline(cin, name);
+    cin.getline(name, 100);
+    // cin >> name;
+    // getline(cin, name);
     cout << "Enter your school : ";
-    getline(cin, school);
+    cin.getline(school, 100);
+    // cin >> school;
+    // getline(cin, school);
     cout << "Your name is " << name << endl;
     cout << "Your school is " << school;
 }
