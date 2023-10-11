81d3036cc8c23ef89ec498f55b0a6400a8b0c38a
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-11 13:22:01 +0700

    format

--- 10/ex101.cpp
+++ 10/ex101.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-float SendPi(){
-float P;
-P = 22.0/7.0;
-return P;
-
+float SendPi()
+{
+    float P;
+    P = 22.0 / 7.0;
+    return P;
 }
 
-main(){
+main()
+{
 
-cout << SendPi();
+    cout << SendPi();
 }
