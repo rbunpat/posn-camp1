0ad84af79b2e2542433006ad9c4a70fad7369f8e
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-11 15:24:14 +0700

    sfgsfdggf

--- 09/peter/grader/than.cpp
+++ 09/peter/grader/than.cpp
@@ -6,10 +6,18 @@ int main() {
     int breadCount, peopleCount, breadPerPerson, peopleFull, breadLeft;
     cin >> breadCount >> peopleCount >> breadPerPerson;
 
+
+
+    if (breadCount == 0 || peopleCount == 0 || breadPerPerson == 0) {
+        cout << peopleCount << " people" << endl;
+
+        cout << breadCount << " left" << endl;
+        return 0;
+    }
+
     peopleFull = breadCount / breadPerPerson;
 
     breadLeft = breadCount % breadPerPerson;
-
     cout << peopleFull << " people" << endl;
 
     cout <<breadLeft << " left" << endl;
