5acbc8e22ce35756eec444b673b2af73248a45d5
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-12 17:53:15 +0700

    inthedirectionofnugget

--- grader/array/nugget.cpp
+++ grader/array/nugget.cpp
@@ -1,33 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-bool a[110];
+bool a[101], b[101];
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+int main() {
+    
     int n;
     cin >> n;
-    a[0] = true;
-    if(n<6){
+
+    a[6] = a[9] = a[20] = true;
+
+    if (n < 6) {
         cout << "no";
+        return 0;
     }
-    for(int i=1; i<=n; i++){
-        if(i>=6){
-            a[i]=a[i]||a[i-6];
+
+    for (int i = 1; i <= 100; i++) {
+        if (i > 6) {
+            if (a[i - 6]) {
+                a[i] = true;
+            }
         }
-        if(i>=9){
-            a[i]=a[i]||a[i-9];
+        if (i > 9) {
+            if (a[i - 9]) {
+                a[i] = true;
+            }
         }
-        if(i>=20){
-            a[i]=a[i]||a[i-20];
+        if (i > 20) {
+            if (a[i - 20]) {
+                a[i] = true;
+            }
+        }
+        if (a[i]) {
+            cout << i << endl;
         }
     }
-    for(int i=1; i<=n; i++){
-        if(a[i]==true){
-            cout << i << "\n";
-        }
-    }
+
     return 0;
 }
