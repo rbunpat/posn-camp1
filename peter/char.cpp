e964184c8c81670654304e6ace9f24efe7e5de09
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-14 16:13:21 +0700

    หกดหกด

---
+++ peter/char.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, i = 0;
+    cin >> n;
+    char str[n], data;
+
+    do
+    {
+        cin >> data;
+        if (isalpha(data)) {
+            str[i] = data;
+            i++;
+        }
+    } while (i < n);
+
+    for (int i = 0; i < n; i++) {
+        cout << str[i] << " ";
+    }
+
+    
+
+    // for (int i = 0; i < n; i++) {
+    //     cin >> str[i];
+    // }
+
+    // for (int i = 0; i < n; i++) {
+    //     if (isalpha(str[i])) {
+    //         << str[i];
+    //         //add to an array
+            
+    //     }
+    // }
+
+    return 0;
+}
