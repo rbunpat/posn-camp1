84fd5066e599b03780db2f38f3681d316c88dabc
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-10-14 11:33:32 +0700

    whyyyyyyyyy

---
+++ pointers/grader/stringcounter.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string originalString;
+    getline(cin, originalString);
+
+    string newString = "";
+
+    // search for unique charaftsrs and pu it in newstring
+
+    for (int i = 0; i < originalString.length(); i++) {
+        bool isUnique = true;
+        for (int j = 0; j < newString.length(); j++) {
+            if (originalString[i] == newString[j] && isalpha(originalString[i]) && originalString[i] != ' ') {
+                isUnique = false;
+                break;
+            }
+        }
+        if (isUnique) {
+            newString += originalString[i];
+        }
+    }
+
+    cout << newString << endl;
+
+    for (int k = 0; k < newString.length(); k++) {
+        int count = 0;
+        for (int l = 0; l < originalString.length(); l++) {
+            if (newString[k] == originalString[l]) {
+                count++;
+            }
+        }
+        cout << newString[k] << " " << count << endl;
+    }
+
+
+    return 0;
+}
